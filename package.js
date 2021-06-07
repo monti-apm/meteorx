21890cce340c31008cae0e8e65fa99c9ea518a0e
zodern <zodern@users.noreply.github.com>
2021-06-07 11:52:22 -0500

    Use api.addFiles

--- package.js
+++ package.js
@@ -13,11 +13,11 @@ Package.onUse(function(api) {
 Package.onTest(function(api) {
   configurePackage(api);
   api.use(["tinytest"], "server");
-  api.add_files(["test/server.js"], "server");
+  api.addFiles(["test/server.js"], "server");
 });
 
 function configurePackage(api) {
   api.versionsFrom("METEOR@1.0");
   api.use(["random", "mongo"], "server");
-  api.add_files(["src/livedata.js", "src/mongo-livedata.js", "src/server.js"], "server");
+  api.addFiles(["src/livedata.js", "src/mongo-livedata.js", "src/server.js"], "server");
 }
